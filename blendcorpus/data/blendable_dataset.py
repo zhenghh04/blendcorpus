61de1cac09e8d6044295dd56270e41f26c305604
Huihuo Zheng <huihuo.zheng@anl.gov>
2025-06-14 00:51:55 -0500

    restructured

---
+++ blendcorpus/data/blendable_dataset.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+
+"""Blendable dataset."""
+
+import hashlib
+import os
+import time
+
+import logging
+import numpy as np
+import torch
+
+# from megatron import print_rank_0
+from blendcorpus import mpu
+from blendcorpus.utils import Profile, PerfTrace, get_logger
+from mpi4py import MPI
+
+log = get_logger(__name__, rank_zero_only=True)
+
+dlp = Profile("DATASET")
+class BlendableDataset(torch.utils.data.Dataset):
+    @dlp.log
+    def __init__(self, datasets, weights, size, *,
+                 data_cache_path=None):
+
+        self.datasets = datasets
+        num_datasets = len(datasets)
+        assert num_datasets == len(weights)
+
+        self.size = size
+
+        # Normalize weights.
+        weights = np.array(weights, dtype=np.float64)
+        sum_weights = np.sum(weights)
+        assert sum_weights > 0.0
+        weights /= sum_weights
+
+        # Build indicies.
+        @dlp.log
+        def _build_indices():
+            start_time = time.perf_counter()
+            dataset_index = np.zeros(self.size, dtype=np.int64)
+            dataset_sample_index = np.zeros(self.size, dtype=np.int64)
+
+            from blendcorpus.data import helpers
+            helpers.build_blending_indices(dataset_index, dataset_sample_index,
+                                           weights, num_datasets, self.size,
+                                           torch.distributed.get_rank() == 0)
+            log.info(
+                "> elapsed time for building blendable dataset indices: "
+                f"{time.perf_counter() - start_time:.2f} (sec)"
+            )
+            return dataset_index, dataset_sample_index
+
+        desc = "Blendable dataset\n\n"
+        desc += "Datasets:\n"
+        for dataset in datasets:
+            desc += dataset.desc + "\n\n"
+        desc += f"Weights: {weights}\n"
+        desc += f"Size: {size}\n"
+        self.desc = desc
+        self.dataset_index = np.zeros(self.size, dtype=np.int64)
+        self.dataset_sample_index = np.zeros(self.size, dtype=np.int64)
+        if data_cache_path:
+            desc_hash = hashlib.md5(desc.encode('utf-8')).hexdigest()
+            desc_path = os.path.join(data_cache_path, desc_hash + ".dsc")
+            index_path = os.path.join(data_cache_path, desc_hash + "_index.npy")
+            sample_index_path = os.path.join(data_cache_path, desc_hash + "_sample_index.npy")
+            cache_hit = os.path.isfile(index_path) and os.path.isfile(sample_index_path)
+            cache_success = True
+            if torch.distributed.get_rank() == 0 and not cache_hit:
+                print(' > WARNING: could not find index map files for blendable'
+                      ' dataset, building indices on rank 0 ...', flush=True)
+                dataset_index, dataset_sample_index = _build_indices()
+                try:
+                    log.debug(" > saving index map files")
+                    start_time = time.perf_counter()
+                    os.makedirs(os.path.dirname(index_path), exist_ok=True)
+                    with open(desc_path, 'wt') as fd:
+                        fd.write(desc)
+                        np.save(index_path, dataset_index, allow_pickle=True)
+                        np.save(sample_index_path, dataset_sample_index,
+                                allow_pickle=True)
+                    log.info(f" > finished saving index map files in {time.perf_counter() - start_time} seconds")
+                except OSError:
+                    print(f'There was an error trying to create the data cache directory ({data_cache_path})')
+                    print('or a file in it. This is set with the --data-cache-path argument. Please')
+                    print('ensure you have write access to this directory or specify one that you do have')
+                    print('write access to.')
+                    cache_success = False
+                self.dataset_index = dataset_index
+                self.dataset_sample_index = dataset_sample_index
+            ''' I don't think the following piece of code is necessary any more; I commented them out now
+            counts = get_accelerator().LongTensor([cache_success])
+            torch.distributed.all_reduce(counts, group=mpu.get_data_parallel_group())
+            torch.distributed.all_reduce(counts, group=mpu.get_pipeline_model_parallel_group())
+            if counts[0].item() != (
+                    torch.distributed.get_world_size() //
+                    torch.distributed.get_world_size(group=mpu.get_tensor_model_parallel_group()) //
+                    torch.distributed.get_world_size(group=mpu.get_sequence_parallel_group())):
+                log.info("Data index creation unsuccessful, exiting.")
+                exit()
+            '''
+            torch.distributed.barrier(group=mpu.get_data_parallel_group())
+            torch.distributed.barrier(group=mpu.get_pipeline_model_parallel_group())
+            torch.distributed.barrier(group=mpu.get_data_parallel_group())
+
+            start_time = time.perf_counter()
+            log.info(f'> loading blendable dataset index: {index_path}')
+            self.dataset_index = np.load(index_path, allow_pickle=True, mmap_mode='r')
+            assert self.dataset_index.size == self.size
+            log.info(f'> loading blendable dataset sample index: {sample_index_path}')
+            self.dataset_sample_index = np.load(sample_index_path, allow_pickle=True, mmap_mode='r')
+            assert self.dataset_sample_index.size == self.size
+            log.info(f'> finished loading in {time.perf_counter() - start_time} seconds')
+        else:
+            self.dataset_index, self.dataset_sample_index = _build_indices()
+
+
+        # Check size
+        _ = self.__getitem__(self.size - 1)
+        try:
+            _ = self.__getitem__(self.size)
+            raise RuntimeError('BlendedDataset size is improperly bounded')
+        except IndexError:
+            pass
+        log.info('> size of blendable dataset: '
+                     '{} samples'.format(self.size))
+
+
+    def __len__(self):
+        return self.size
+
+    @dlp.log
+    def __getitem__(self, idx):
+        dataset_idx = self.dataset_index[idx]
+        sample_idx = self.dataset_sample_index[idx]
+        return {
+            "dataset_idx" : dataset_idx,
+            **self.datasets[dataset_idx][sample_idx],
+        }
---
+++ tests/test_dataloader.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+import torch
+import time
+import json
+start_time = time.time()
+from mpi4py import MPI
+import os
+import numpy as np
+from blendcorpus import (
+    get_config, 
+    set_config, 
+    mpu, 
+    build_gpt_datasets, 
+    build_pretraining_data_loader
+)
+import argparse
+import datetime
+
+comm = MPI.COMM_WORLD
+def print_rank_0(msg):
+    if comm.rank==0:
+        print(f" [INFO][{datetime.datetime.now()}] {msg}", flush=True)
+end_time = time.time()        
+print_rank_0(f"Loaded python modules in {end_time - start_time} seconds") 
+
+def main():
+    ## initialize parallel degree
+    mpu.initialize_model_parallel(
+        tensor_model_parallel_size=1,
+        pipeline_model_parallel_size=1,
+        sequence_parallel_size=1,
+    )
+
+    from blendcorpus.utils import PerfTrace, Profile
+
+    comm.Barrier()
+    print_rank_0(f"Barrier synchonization time:  {time.time() - end_time} seconds")
+    def get_args():
+        parser = argparse.ArgumentParser(description="Test dataloader with MPI")
+        parser.add_argument("--trace-dir", dest="trace_dir", required=True,
+                            help="Directory to write performance traces")
+        parser.add_argument("--data-file-list", dest="data_file_list", required=True,
+                            help="Path to the file listing datasets and weights")
+        parser.add_argument("--global-batch-size", dest="global_batch_size", type=int, required=True,
+                            help="Global batch size for sampling")
+        parser.add_argument("--train-iters", dest="train_iters", type=int, required=True,
+                            help="Number of training iterations to simulate")
+        parser.add_argument("--seed", type=int, default=0, help="Random seed")
+        parser.add_argument("--data-impl", dest="data_impl", default="mmap",
+                            help="Data implementation method (e.g., mmap or lazy)") 
+        parser.add_argument("--mmap-warmup", action="store_true",
+                            help="Whether to perform mmap warmup")
+        parser.add_argument("--seq-length", dest="seq_length", type=int, required=True,
+                            help="Sequence length for datasets")
+        parser.add_argument("--data-cache-path", dest="data_cache_path", default="./data_cache",
+                            help="Path to cache data")
+        parser.add_argument("--consumed-train-samples", dest="consumed_train_samples", type=int, default=0,
+                            help="Number of already consumed train samples")
+        parser.add_argument("--consumed-valid-samples", dest="consumed_valid_samples", type=int, default=0,
+                            help="Number of already consumed valid samples")
+        parser.add_argument("--dataloader-type", dest="dataloader_type",
+                            choices=["single", "cyclic"], default="single",
+                            help="Type of dataloader to use: 'single' or 'cyclic'")
+        parser.add_argument("--micro-batch-size", dest="micro_batch_size",
+                            type=int, default=4,
+                            help="Micro batch size for each data parallel rank")
+        parser.add_argument("--data-sharding", dest="data_sharding",
+                            action="store_true",
+                            help="Enable data sharding for cyclic sampler")
+        parser.add_argument("--num-workers", dest="num_workers",
+                            type=int, default=2,
+                            help="Number of worker processes for DataLoader")
+        parser.add_argument("--multiprocessing-context", dest="multiprocessing_context",
+                            default="spawn",
+                            help="Multiprocessing context for DataLoader workers")
+        parser.add_argument("--repeated-dataloader", dest="repeated_dataloader",
+                            action="store_true",
+                            help="Wrap DataLoader in RepeatingLoader if set")
+        return parser.parse_args()
+
+    args = get_args()
+    set_config(args)
+    config = get_config()
+    os.makedirs(args.trace_dir, exist_ok=True)
+    # Build datasets
+    start_build_dataset = time.time()
+
+    print_rank_0(f"Starting to build the blendable dataset")
+    train_ds, valid_ds, test_ds = build_gpt_datasets(config)
+    end_build_dataset = time.time()
+    print_rank_0(f"Finished building the blendable dataset in {end_build_dataset - start_build_dataset} second")
+    print_rank_0(f"Total number of samples: {len(train_ds)} {len(valid_ds)} {len(test_ds)}")
+
+    def get_sample_info(blendable_dataset, idx):
+        # corpus dataset
+        cd = blendable_dataset.dataset_index[idx]
+        # index within the corpus dataset
+        cds = blendable_dataset.dataset_sample_index[idx]
+        # dataset index within each corpus
+        fcd = blendable_dataset.datasets[cd].dataset_index[cds]
+        # sample index within the dataset
+        fcds = blendable_dataset.datasets[cd].dataset_sample_index[cds]
+        # corresponding data file
+        prefix = blendable_dataset.datasets[cd].dataset_builders[fcd].prefix
+        corpus = blendable_dataset.datasets[cd].dataset_builders[fcd].corpus
+        #v = blendable_dataset[idx]['text']
+        #norm = np.linalg.norm(v)
+        return prefix, corpus, fcds
+
+    num_batches =  args.train_iters
+    print_rank_0(f"global_batch_size: {args.global_batch_size}")
+    print_rank_0(f"number of batches: {num_batches}")
+
+    #-------
+    files = []
+    weights = []
+    flist = []
+    corpus_all = []
+    with open(args.data_file_list, 'r') as fin:
+        for f in fin.readlines():
+            w, fname, c = f.split()
+            weights.append(float(w))
+            flist.append(fname)
+            files.append(float(w))
+            files.append(fname)
+            files.append(c)
+            if c not in corpus_all:
+                corpus_all.append(c)
+    # ---- 
+    fout = open("samples_list.jsonl", "w")
+    if comm.rank == 0:
+        for i in range(num_batches):
+            ns_corpus = {}
+            for c in corpus_all:
+                ns_corpus[c] = 0
+            for j in range(args.global_batch_size):
+                prefix, corpus, idx = get_sample_info(train_ds, i*args.global_batch_size+j)
+                ns_corpus[corpus] +=1
+                fout.write(f"\u007b 'batch': {i}, 'sample': {j}, 'corpus': '{corpus}', 'prefix': '{prefix}', 'dataset_sample_index': {idx} \u007d\n")
+            fout.write(f"\u007b 'batch': {i}, 'histogram': {ns_corpus} \u007d \n")
+    comm.Barrier()        
+    start_build_dataloader = time.time()
+    print_rank_0(f"Starting to build the data loader")
+    rank_in_parallel_group = mpu.get_sequence_parallel_rank()
+
+    print_rank_0(f"rank_in_parallel_group: {rank_in_parallel_group}")
+    train_dataloader = build_pretraining_data_loader(
+        train_ds, args.consumed_train_samples, config)
+    print_rank_0(f"train done")
+    valid_dataloader = build_pretraining_data_loader(
+        valid_ds, args.consumed_valid_samples, config)
+    test_dataloader = build_pretraining_data_loader(test_ds, 0, config)
+    end_build_dataloader = time.time()
+    print_rank_0(f"Finished building the data loader in {end_build_dataloader - start_build_dataloader} second")
+
+    print_rank_0(f"Starting loading the data")
+    start_loading_time = time.time()
+    NUM_ITEMS=1
+    SLEEP_TIME=10.0
+    def compute(ct):
+        time.sleep(ct)
+    n=0
+    start_time = time.time()
+    for i in iter(train_dataloader):
+        print(f"[{comm.rank}] DATA {i}")
+        n+=1
+        if (n%NUM_ITEMS==0):
+            print_rank_0(f"Proccessed {n}th-batch in {time.time() - start_time}")
+        if n>=1000:
+            break
+        start_time = time.time()
+    end_loading_time = time.time()
+    print_rank_0(f"Finished loading the data ({n} batches) in {end_loading_time - start_loading_time}")
+
+
+# Entrypoint for script execution
+if __name__ == "__main__":
+    from torch_setup import init_distributed
+    init_distributed()
+    main()
